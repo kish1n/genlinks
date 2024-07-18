2c04a1e024bef43f711f560997037ecd88692fb1
kish1n <yehorushakov@gmail.com>
2024-07-18 16:46:37 +0300

    add handlers

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/kish1n/genlinks
 go 1.16
 
 require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
 )
--- pakage/service/handlers/handlers.go
+++ pakage/service/handlers/handlers.go
@@ -1,1 +1,25 @@
 package handlers
+
+import (
+	"context"
+	"log"
+	"os"
+)
+
+// Определение ключа для хранения логгера в контексте
+type ctxKeyLog struct{}
+
+// CtxLog возвращает функцию, добавляющую логгер в контекст
+func CtxLog(log *log.Logger) func(context.Context) context.Context {
+	return func(ctx context.Context) context.Context {
+		return context.WithValue(ctx, ctxKeyLog{}, log)
+	}
+}
+
+// LogFromCtx извлекает логгер из контекста
+func LogFromCtx(ctx context.Context) *log.Logger {
+	if log, ok := ctx.Value(ctxKeyLog{}).(*log.Logger); ok {
+		return log
+	}
+	return log.New(os.Stdout, "default: ", log.LstdFlags)
+}
