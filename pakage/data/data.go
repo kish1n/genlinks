b0ea584a6ff77f592845fbadf024aa8a90d063a1
kish1n <yehorushakov@gmail.com>
2024-07-19 15:25:04 +0300

    add env

--- pakage/data/data.go
+++ pakage/data/data.go
@@ -5,12 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func initDB() (*sql.DB, error) {
-	connStr := "postgres://postgres:1029@localhost/postgres?sslmode=disable"
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"))
 
 	fmt.Println("connStr: ", connStr)
 
